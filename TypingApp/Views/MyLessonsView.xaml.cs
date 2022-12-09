15982387a756776d5cea7c381535a2a66027e1ed
AJ Vossebelt <bertilvossebelt@gmail.com>
2022-12-10 00:05:31 +0100

    MyLessonsView added

---
+++ TypingApp/Views/MyLessonsView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace TypingApp.Views;
+
+public partial class MyLessonsView : UserControl
+{
+    public MyLessonsView()
+    {
+        InitializeComponent();
+    }
+}
